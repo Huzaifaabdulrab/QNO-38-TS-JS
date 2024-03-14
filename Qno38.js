ad387924cba14c6e0ae8f695b30596a2438254ef
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:59:34 +0500

    Add files via upload

---
+++ Qno38.js
@@ -0,0 +1,9 @@
+"use strict";
+// 38. Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.
+Object.defineProperty(exports, "__esModule", { value: true });
+function city_name(city = "", country = "") {
+    console.log(`${city} is in ${country}`);
+}
+city_name("Karachi", "Pakistan");
+city_name("Paris", "London");
+city_name("Aghra", "India");
